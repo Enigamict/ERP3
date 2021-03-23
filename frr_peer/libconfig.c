e3e59900f0fbede0cd4e31b9947ed980b4f451b6
enigamict <ghuhumei@yahoo.co.jp>
2021-03-24 02:02:40 +0900

    ‘json’

--- frr_peer/libconfig.c
+++ frr_peer/libconfig.c
@@ -6,7 +6,7 @@ int main()
 {
   struct config cfg;
   memset(&cfg, 0, sizeof(cfg));
-  int ret = config_parse(&cfg,"./r1.json");
+  int ret = config_parse(&cfg,"./test.json");
   if (ret < 0) {
     fprintf(stderr, "failed on config parser\n");
     return 1;
